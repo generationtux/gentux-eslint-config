b1bf8a77f764b87a2dfb9268bcc89ac7899fbbaa
Chris Burgin <chrisburgin95@gmail.com>
2018-03-14 09:41:35 -0400

    Typescript Support

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 module.exports = {
   extends: "react-app",
-  plugins: ["jsx-a11y"]
+  plugins: ["jsx-a11y"],
+  overrides: {
+    files: ["**/*.ts", "**/*.tsx"],
+    parser: "typescript-eslint-parser",
+    rules: {
+      "no-unused-vars": [0],
+      "no-undef": [0]
+    }
+  }
 };
