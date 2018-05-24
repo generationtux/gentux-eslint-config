537ce14c6a76aec29f24a9f437d047eb56847421
Chris Burgin <chrisburgin95@gmail.com>
2018-05-24 14:44:19 -0400

    Updated Eslint Config with React Stuff

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 module.exports = {
   extends: "react-app",
-  plugins: ["jsx-a11y"],
+  plugins: ["jsx-a11y", "react"],
   rules: {
-    "no-console": 1
+    "no-console": 1,
+    "react/no-access-state-in-setstate": "error",
+    "react/no-redundant-should-component-update": "error",
+    "react/no-direct-mutation-state": "error",
+    "react/no-typos": "error",
+    "react/no-unused-state": "error"
   },
   overrides: {
     files: ["**/*.ts", "**/*.tsx"],
